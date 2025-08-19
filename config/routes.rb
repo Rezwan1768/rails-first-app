0907146135243b3408b7f7ec843aecf503e39af8
Rezwan <rezwan1768@gmail.com>
2025-08-19 09:50:28 -0400

    Create new product

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
   # resources :products
-  
-  get "products" =>  "products#index"
-  get "products/new" => "products#new"
- get "products/:id" => "products#show", as: :product
+  root "products#index"
+  get "new" => "products#new"
+  get ":id" => "products#show", as: :product
+  post "hello" => "products#create"
   # Defines the root path route ("/")
   # root "posts#index"
 end
